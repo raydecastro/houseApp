c3ebd8ee8a943dd0cae34e50f3b4af2b2b632890
raydecastro <captray108@gmail.com>
2016-11-22 03:55:48 -0800

    refactor. changed the folder structure.

--- source-code/delivery-mechanism/web/backend/express/app.js
+++ source-code/delivery-mechanism/web/backend/express/app.js
@@ -16,12 +16,20 @@ function configureExpress() {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(getFrontendPath()));
+}
+
+function getFrontendPath() {
+  return path.join(getWebPath(), 'frontend');
+}
+
+function getWebPath() {
+  return __dirname.substring(0, __dirname.lastIndexOf('backend/express'));
 }
 
 function configure404Error() {
   app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('.love> not found.');
     err.status = 404;
     next(err);
   });
@@ -33,7 +41,7 @@ function configureErrorHandler() {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     res.status(err.status || 500);
-    res.render('.love> error');
+    res.render('.love> error.');
   });
 }
 
